7ce4af359b2c4df8aba9f395f1ad3c6f28832dc9
Is2dann <b.daniel.oliver@gmail.com>
2025-06-29 16:15:47 +0100

    Changed SECRET KEY for mor security

--- iceman_media/settings.py
+++ iceman_media/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dbitbi&&&u(zmdcx0tgz_0qo567_6)xz*+8sywawjbf(2o4owu'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
