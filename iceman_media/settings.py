2a2c9675f4325926c1ddb3ce1a5b27f8256c406c
Is2dann <b.daniel.oliver@gmail.com>
2025-06-29 15:43:43 +0100

    Created superuser

--- iceman_media/settings.py
+++ iceman_media/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dbitbi&&&u(zmdcx0tgz_0qo567_6)xz*+8sywawjbf(2o4owu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
