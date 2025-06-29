2a517f7c40dbb4806fa3cf95d4902b840a746dc3
Is2dann <b.daniel.oliver@gmail.com>
2025-06-29 23:46:09 +0100

    Add registartion form to index.html. Installed crispyforms for later use.

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.contrib import messages
+from .forms import CustomRegisterForm
 
 
 # Create your views here.
@@ -7,3 +10,19 @@ from django.shortcuts import render
 def index(request):
     
     return render(request, 'home/index.html', )
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatic login after registration.
+            messages.success(request, 'Hooray...Registration successful. Welcome and Thank You')
+            return redirect('index')
+        else:
+            messages.error(request, 'Something is not right. Please check all the fields again.')
+    else:
+        form = CustomRegisterForm()
+
+    return render(request, 'home/index.html', {'form': form})
--- iceman_media/settings.py
+++ iceman_media/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1',]
 
@@ -44,8 +44,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'home',
 ]
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
